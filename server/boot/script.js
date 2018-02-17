d908ff8ed5d13476b7a0bc37bb26d9d1ed768250
Viktor Sivulskiy <svicvg@gmail.com>
2018-02-17 09:57:05 +0600

    Loopback Relations

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,47 +1,47 @@
-module.exports = function(app) {
+// module.exports = function(app) {
 
-    var Customer = app.models.Customer;
+//     var Customer = app.models.Customer;
 
-    Customer.findOne({ username: 'Admin' }, (err, users) => {
+//     Customer.findOne({ username: 'Admin' }, (err, users) => {
 
-        if (!users) {
-            Customer.create([
-                {username: 'Admin', email: 'admin@confusion.net', password: 'password'}
-            ], (err, users) => {
-                if (err) throw(err);
+//         if (!users) {
+//             Customer.create([
+//                 {username: 'Admin', email: 'admin@confusion.net', password: 'password'}
+//             ], (err, users) => {
+//                 if (err) throw(err);
                 
-                var Role = app.models.Role;
-                var RoleMapping = app.models.RoleMapping;
+//                 var Role = app.models.Role;
+//                 var RoleMapping = app.models.RoleMapping;
 
-                RoleMapping.destroyAll();
+//                 RoleMapping.destroyAll();
 
-                Role.findOne({name: 'admin'}, (err, role) => {
+//                 Role.findOne({name: 'admin'}, (err, role) => {
 
-                    if (!role) {
-                        Role.create({
-                            name: 'admin'
-                        }, (err, role) => {
-                            if (err) throw(err);
-                            console.log("New Role: ", role);
+//                     if (!role) {
+//                         Role.create({
+//                             name: 'admin'
+//                         }, (err, role) => {
+//                             if (err) throw(err);
+//                             console.log("New Role: ", role);
             
-                            role.principals.create({
-                                principalType: RoleMapping.USER,
-                                principalId: users[0].id
-                            }, (err, principal) => {
-                                if (err) throw(err);
-                            });
-                        });
-                    }
-                    else {
-                        role.principals.create({
-                            principalType: RoleMapping.USER,
-                            principalId: users[0].id
-                        }, (err, principal) => {
-                            if (err) throw(err);
-                        });                            
-                    }
-                })
-            });
-        }
-    });
-}
+//                             role.principals.create({
+//                                 principalType: RoleMapping.USER,
+//                                 principalId: users[0].id
+//                             }, (err, principal) => {
+//                                 if (err) throw(err);
+//                             });
+//                         });
+//                     }
+//                     else {
+//                         role.principals.create({
+//                             principalType: RoleMapping.USER,
+//                             principalId: users[0].id
+//                         }, (err, principal) => {
+//                             if (err) throw(err);
+//                         });                            
+//                     }
+//                 })
+//             });
+//         }
+//     });
+// }
